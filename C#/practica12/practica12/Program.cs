bbdae88717cb32ead917f5cb9a98752b12d91c20
esvivas <esvivas@gmail.com>
2022-03-04 11:41:23 -0500

    form soldados

---
+++ C#/practica12/practica12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica12
+{
+    internal class Program
+    {
+        enum baraja { CORAZONES, DIAMANTES, TREBOLES, PICAS};
+        static void Main(string[] args)
+        {
+            baraja cartas = baraja.CORAZONES;
+            string color;
+            switch (cartas)
+            {
+                case baraja.CORAZONES:
+                    color = "ROJO";
+                    break;
+                case baraja.DIAMANTES:
+                    color = "ROJO";
+                    break;
+                case baraja.TREBOLES:
+                    color = "NEGRO";
+                    break;
+                case baraja.PICAS:
+                    color = "NEGRO";
+                    break;
+                default: 
+                    color = "blanco";
+                    break;
+            }
+
+            Console.WriteLine("Su carta "+cartas +" es de color " + color);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/proyecto14/proyecto14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciclo 1");
+            for(int k = 0; k <= 10; k++)
+            {
+                Console.Write("\t k = " + k);
+            }
+            Console.WriteLine("\nCiclo 2");
+            int p = 1;
+            for (; p <= 10; p++)
+            {
+                Console.Write("\t p = " + p);
+            }
+            Console.WriteLine("\nCiclo 3");
+            int x = 1;
+            for (; x <= 10;)
+            {
+                Console.Write("\t x = " + x);
+                x++;
+            }
+            Console.WriteLine("\nCiclo 4");
+            int y = 1;
+            for (; ;)
+            {   
+                if (y <= 10)
+                {
+                    Console.Write("\t y = " + y);
+                    y++;
+                }
+            }
+
+            Console.WriteLine("\n Ciclo 5");
+            for (int i = 0; i <= 10; Console.Write("\t i = " + i++) );
+
+        }
+    }
+}
