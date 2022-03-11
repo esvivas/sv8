651c1c6a9be684647489b5859469a8839c378aae
esvivas <esvivas@gmail.com>
2022-03-11 10:55:06 -0500

    retorno inmediato

---
+++ C#/practica21/practica21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            saludar(500);
+            Console.ReadKey();
+        }
+
+        public static void saludar(int dinero)
+        {
+            if(dinero < 200)
+            {
+                return;
+            }
+            Console.WriteLine("A cómo el pandebono... a 1500 con tinto");
+        }
+    }
+}
