9002df57ce9a3e93a61f6a7234e6ec8baaa3fbb3
esvivas <esvivas@gmail.com>
2022-03-18 12:35:24 -0500

    CRUD

---
+++ C#/practica30/practica30/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace practica30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.btn_consultar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_nombre
+            // 
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nombre.Location = new System.Drawing.Point(82, 58);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(52, 16);
+            this.lbl_nombre.TabIndex = 0;
+            this.lbl_nombre.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(98, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(140, 58);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(254, 20);
+            this.txt_nombre.TabIndex = 2;
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Location = new System.Drawing.Point(140, 116);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(68, 20);
+            this.txt_edad.TabIndex = 3;
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(80, 183);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_registrar.TabIndex = 4;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            this.btn_registrar.Click += new System.EventHandler(this.btn_registrar_Click);
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.Location = new System.Drawing.Point(201, 183);
+            this.btn_consultar.Name = "btn_consultar";
+            this.btn_consultar.Size = new System.Drawing.Size(75, 23);
+            this.btn_consultar.TabIndex = 5;
+            this.btn_consultar.Text = "Consultar";
+            this.btn_consultar.UseVisualStyleBackColor = true;
+            this.btn_consultar.Click += new System.EventHandler(this.btn_consultar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(319, 183);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 6;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(498, 290);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_consultar);
+            this.Controls.Add(this.btn_registrar);
+            this.Controls.Add(this.txt_edad);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.Button btn_consultar;
+        private System.Windows.Forms.Button btn_limpiar;
+    }
+}
+
---
+++ C#/practica30/practica30/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica30
+{
+    public partial class Form1 : Form
+    {
+        Estudiante estudiante;
+        public Form1()
+        {
+            InitializeComponent();
+            estudiante = new Estudiante();
+        }
+
+        private void btn_registrar_Click(object sender, EventArgs e)
+        {
+            estudiante.Nombre = txt_nombre.Text;
+            estudiante.setEdad(txt_edad.Text);
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_nombre.Clear();
+            txt_edad.Clear();
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            txt_nombre.Text = estudiante.Nombre;
+            txt_edad.Text = estudiante.getEdad().ToString();
+        }
+    }
+
+    class Estudiante
+    {
+        private string nombre;
+        private int edad;
+
+        //Por propiedad
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        //Método set
+        public void setEdad(String n)
+        {
+            int numero = Convert.ToInt32(n);
+            edad = numero;
+        }
+
+        //Método get
+        public int getEdad()
+        {
+            return edad;
+        }
+
+    }
+}
